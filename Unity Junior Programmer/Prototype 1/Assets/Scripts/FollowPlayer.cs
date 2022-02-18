5ae519c4350bb2bca464536e082c8b7690d13d31
Josh <joshg4569@gmail.com>
2022-02-18 09:10:50 -0800

    L1.4 Completed

--- Unity Junior Programmer/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Unity Junior Programmer/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,5,-7);
     // Update is called once per frame
-    public float speed = 5.0f;
-    void Update()
+    void LateUpdate()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
